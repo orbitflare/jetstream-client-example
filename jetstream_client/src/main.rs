9cdd19e5d4366761c9bee57a60f9ac88f6909e53
Taimoor <taimoorshafique541@gmail.com>
2025-05-06 23:30:09 +0500

    update: protobufs and auth supports

--- jetstream_client/src/main.rs
+++ jetstream_client/src/main.rs
@@ -1,22 +1,20 @@
+mod connector;
 use clap::Parser;
 use env_logger::Env;
 
-use jetstream_client_example::config::ClientConfig;
-use jetstream_client_example::jetstream_connector;
+use connector::config::ClientConfig;
+use connector::jetstream_connector;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    // Set up simple logging
     env_logger::Builder::from_env(Env::default().default_filter_or("info"))
         .format_timestamp_secs()
         .init();
 
-    // Parse command-line arguments
     let config = ClientConfig::parse();
 
     log::info!("Starting JetStream Example Client");
 
-    // Connect to JetStream and process transactions
     jetstream_connector(config).await?;
 
     Ok(())
